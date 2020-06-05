9c574b0bffb5ec14ce637cdb098d37d2c8603c7d
Michael Suleiman <mosuleiman19@gmail.com>
2020-06-05 13:29:42 +0400

    Minor changes to how injectable is used

--- lib/injection.iconfig.dart
+++ lib/injection.iconfig.dart
@@ -8,9 +8,9 @@ import 'package:ddd_notes/infrastructure/core/firebase_injectable_module.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:ddd_notes/application/auth/sign_in_form/sign_in_form_bloc.dart';
-import 'package:ddd_notes/domain/auth/i_auth_facade.dart';
 import 'package:ddd_notes/infrastructure/auth/firebase_auth_facade.dart';
+import 'package:ddd_notes/domain/auth/i_auth_facade.dart';
+import 'package:ddd_notes/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 void $initGetIt(GetIt g, {String environment}) {
@@ -20,9 +20,9 @@ void $initGetIt(GetIt g, {String environment}) {
   g.registerLazySingleton<Firestore>(() => firebaseInjectableModule.firestore);
   g.registerLazySingleton<GoogleSignIn>(
       () => firebaseInjectableModule.googleSignIn);
-  g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
-  g.registerLazySingleton<FirebaseAuthFacade>(
+  g.registerLazySingleton<IAuthFacade>(
       () => FirebaseAuthFacade(g<FirebaseAuth>(), g<GoogleSignIn>()));
+  g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
 }
 
 class _$FirebaseInjectableModule extends FirebaseInjectableModule {}
