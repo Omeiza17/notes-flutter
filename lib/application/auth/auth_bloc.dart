60273b584318361ca562c403280c6eeb50d08fe6
Michael Suleiman <mosuleiman19@gmail.com>
2020-06-05 14:55:03 +0400

    Chapter: 10 - Navigation based on Authentication state

--- lib/application/auth/auth_bloc.dart
+++ lib/application/auth/auth_bloc.dart
@@ -7,9 +7,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 
 part 'auth_bloc.freezed.dart';
-
 part 'auth_event.dart';
-
 part 'auth_state.dart';
 
 @injectable
